aefa8a5446ea92a32eb0dedda36fb9fab30848bd
S1rShadez <arthure97@hotmail.com>
2023-10-06 14:55:03 +0200

    created login and register pages, changed css for both pages

--- js/loginRegister/controller.js
+++ js/loginRegister/controller.js
@@ -1,14 +1,43 @@
 //verify username and password and send the user to the dashboard
- let storedUserName = model.data.user.userName;
- let storedPassword = model.data.user.password;
+let storedUserName = model.data.user.userName;
+let storedPassword = model.data.user.password;
 
-function login(){
+function login() {
     let username = document.getElementById('uNameIn').value;
     let password = document.getElementById('pWordIn').value;
 
-    if(username == storedUserName && password == storedPassword){
-        
+    if (username == storedUserName && password == storedPassword) {
+        //send user to dashboard
     }
 }
 
 //send user to register page
+function sendToRegisterUser() {
+    registerUserView();
+}
+
+//send user back to login page
+function backToLogin() {
+    loginView();
+}
+
+//function to create user account
+function createUser() {
+    let username = document.getElementById('usernameInput').value;
+    let email = document.getElementById('emailInput').value;
+    let password = document.getElementById('passwordInput').value;
+    let passwordRepeat = document.getElementById('passwordInputRepeat').value;
+
+    model.data.user.userName = username;
+    model.data.user.email = email;
+    if (password === passwordRepeat) {
+        model.data.user.password = password;
+    }
+
+    document.getElementById('usernameInput').value = '';
+    document.getElementById('emainInput').value = '';
+    document.getElementById('passwordInput').value = '';
+    document.getElementById('passwordInputRepeat').value = '';
+
+    loginView();
+}
