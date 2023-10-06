3ff74cf477f846c6179617053a3c2418868dcfe3
lefIngridL <90441834+lefIngridL@users.noreply.github.com>
2023-10-06 13:58:57 +0200

    innit

--- js/addtaskOrGoalTab/view.js
+++ js/addtaskOrGoalTab/view.js
@@ -3,9 +3,39 @@
 
 //updateView();
 function updateViewAddTab(){
+    let theme = model.interface.addGoalAct.themeSelector;
     html = /*html*/ `
-    
+    <form action="...">
+    <label for="selectTheme">NK tema:</label>
+    <select name="NKTemaer" id="keyThemes">
+        
+    </select>
+    <br><br>
+    <input type="submit" value="Submit">
+</form>
+
+    <div></div>
+    <div></div>
+    <div></div>
     `;
     app.innerHTML = html;
+for(i in theme){
+    document.getElementById("keyThemes").innerHTML += `<option value=${theme[i]}>${theme[i]}</option>`;
 }
+
+}
+
+   /*
+        <form action="...">
+            <label for="nøkkelkompetanser">Nøkkelkompetanser:</label>
+            <select name="nøkkelkompetanser" id="keySkills">
+                <option value="jog">Joggetur</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+            </select>
+            <br><br>
+            <input type="submit" value="Submit">
+        </form>
+    `;*/
 
