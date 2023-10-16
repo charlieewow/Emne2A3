2fa6ab14a951a131ab716bca98320252c5eab8cd
S1rShadez <arthure97@hotmail.com>
2023-10-16 14:35:50 +0200

    created goalstab and made some other changes

---
+++ js/goalsTab/controller.js
@@ -0,0 +1,19 @@
+let goalsList = model.data.goalList;
+
+function loadGoals(selection){
+    if(selection.value == 'all'){
+        //console.log('all');
+    }
+    else if(selection.value == 'year'){
+        //console.log('year');
+    }
+    else if(selection.value == 'halfYear'){
+        //console.log('halfYear');
+    }
+    else if(selection.value == 'quarterYear'){
+        //console.log('quarterYear');
+    }
+    else if(selection.value == 'month'){
+        //console.log('month');
+    }
+}
