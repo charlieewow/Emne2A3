243863e435f3e67d540a308fc1ee6326ffd40857
simenbs <141590049+simenbs@users.noreply.github.com>
2023-10-17 12:47:39 +0200

    aktivitetercss

--- js/activityTab/controller.js
+++ js/activityTab/controller.js
@@ -1,21 +1,3 @@
-/*function loadActivity() {
-    let myOnceTasks = model.data.plannedActList.once;
-    let myRepeatTasks = model.data.plannedActList.repeat;
-    let onceArrayTheme = [];
-    let onceArrayName = [];
-    let repeatArrayName = [];
-    console.log(myOnceTasks.length)
-    for (let i = 0; i < myOnceTasks.length; i++) {
-        onceArrayName.push(myOnceTasks[i + 1].name);
-
-        document.getElementById('keySkillAdd').innerHTML += `<div>${onceArrayName[i]}</div>`;
-
-    }
-    for (let i = 0; i < myRepeatTasks.length; i++) {
-        repeatArrayName.push(myRepeatTasks[i + 1].name);
-        document.getElementById('keySkillAdd').innerHTML += `<div>${repeatArrayName[i]}</div>`;
-    }
-}*/
 function loadActivity() {
     let myOnceTasks = model.data.plannedActList?.once;
     let myRepeatTasks = model.data.plannedActList?.repeat;
--- js/activityTab/view.js
+++ js/activityTab/view.js
@@ -1,7 +1,8 @@
 function activityView() {
-    let activity =  model.interface.activity.selectedNK;
+    let activity = model.interface.activity.selectedNK;
     let app = document.getElementById("app");
     let html = /*HTML*/` 
+<<<<<<< Updated upstream
     <button onclick="updateViewAddTab()">Legg til oppgave</button>
     <button onclick="updateViewDash()">Tilbake til Dash</button>
         <form action="...">
@@ -15,11 +16,48 @@ function activityView() {
         </form>
         <div id="keySkillAdd"></div>
         
+=======
+        <div class="wrapper transform border activitywrapper">
+            <img src="img/activities.png" id="activitiesimg">
+            <div class="inneractivitywrapper">
+                <div class="activitynavbar flex">
+                    <form action="...">
+                        <label for="nøkkelkompetanser">Nøkkelkompetanser:</label>
+                        <select name="nøkkelkompetanser" id="keySkills">
+    
+    
+                        </select>
+                    </form>
+                    <div id="keySkillAdd"></div>
+                    <div class="circleplus flex center" onclick="updateViewAddTab()">+</div>
+                </div>
+                <hr>
+                <div class="">
+                    <div class="flex activitiesTheme border">
+                        <div><p>Trygghet</p></div>
+                        <div class="activityprog"><progress class="prog border" id="" value="50" max="100"></progress></div>
+                    </div>
+                    <div class="flex activitiesTheme border">
+                        <div><p>Trygghet</p></div>
+                        <div class="activityprog"><progress class="prog border" id="" value="50" max="100"></progress></div>
+                    </div>
+                    <div class="flex activitiesTheme border">
+                        <div><p>Trygghet</p></div>
+                        <div class="activityprog"><progress class="prog border" id="" value="50" max="100"></progress></div>
+                    </div>
+                    <div class="flex activitiesTheme border">
+                        <div><p>Trygghet</p></div>
+                        <div class="activityprog"><progress class="prog border" id="" value="50" max="100"></progress></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+>>>>>>> Stashed changes
     `;
     app.innerHTML = html;
 
-    for(i in activity){
+    for (i in activity) {
         document.getElementById("keySkills").innerHTML += `<option value=${activity[i]}>${activity[i]}</option>`;
-    }    
+    }
 }
 //updateView();
