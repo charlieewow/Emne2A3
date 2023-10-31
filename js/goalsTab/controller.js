28f609eb43922f0f63f84e68585144fc2e900fe2
S1rShadez <arthure97@hotmail.com>
2023-10-31 14:35:49 +0100

    goals tab can now load goals

--- js/goalsTab/controller.js
+++ js/goalsTab/controller.js
@@ -1,99 +1,39 @@
+let selectedGoal = 'all';
+let goalsData = model.data.goalList;
 
-//let timeframe = 'all';
-
-function processGoals(object, form) {
-    console.log(object.mål);
-    loadGoals(object.mål);
+function selectGoal(selection) {
+    selectedGoal = selection.value;
+    loadGoals()
 }
 
-function selectTimeframe(selection) {
-    /*if(selection.value == 'all'){
-        console.log('all');
-        timeframe = 'all';
-    }
-    else if(selection.value == 'year'){
-        console.log('year');
-        timeframe = 'year';
-    }
-    else if(selection.value == 'halfYear'){
-        //console.log('halfYear');
-        timeframe = 'halfYear';
-    }
-    else if(selection.value == 'quarterYear'){
-        //console.log('quarterYear');
-        timeframe = 'quarterYear';
-    }
-    else if(selection.value == 'month'){
-        //console.log('month');
-        timeframe = 'month';
-    }
-    goalsView();*/
-}
+function loadGoals() {
+    let loadedGoals = '';
 
-function loadGoals(selectedTimeframe) {
-    let goalsList = model.data.goalList;
-    console.log(goalsList);
-    let showGoals = '';
-    console.log(selectedTimeframe);
-    let unit;
-    if (selectedTimeframe !== 'Alle') {
-        for (let i = 1; i < goalsList[selectedTimeframe].length; i++) {
-            console.log("runde" + 1);
-            showGoals += `<div class="flex activitiesTheme border">
-        <div><p> ${goalsList[selectedTimeframe][i].name}</p></div>
-        <div class="activityprog"><progress class="prog border" id="" value="50" max="100"></progress></div>
-        </div>`;
-            console.log(goalsList[selectedTimeframe][i]);
+    if (selectedGoal == 'daily' || selectedGoal == 'all') {
+        for (let i = 0; i < goalsData.daily.length; i++) {
+            loadedGoals += `<div>${goalsData.daily[i].name}</div>`
         }
     }
-    else if (selectedTimeframe == 'Alle') {
-        console.log("true");
-        for (let i = 1; i < goalsList.daily.length; i++) {
-            console.log("runde" + 1);
-            showGoals += `<div class="flex activitiesTheme border">
-             <div><p>${goalsList.daily[i].name}</p></div>
-             <div class="activityprog"><progress class="prog border" id="" value="50" max="100"></progress></div>
-             </div>`;
-        }
-        for (let i = 1; i < goalsList.weekly.length; i++) {
-            console.log("runde" + 1);
-            showGoals +=
-                `<div class="flex activitiesTheme border">
-                         <div><p>${goalsList.weekly[i].name} </p></div>
-                         <div class="activityprog"><progress class="prog border" id="" value="50" max="100"></progress></div>
-                         </div>`;
-        }
-        for (let i = 1; i < goalsList.monthly.length; i++) {
-            console.log("runde" + 1);
-            showGoals += `<div class="flex activitiesTheme border">
-             <div><p> ${goalsList.monthly[i].name}  </p></div>
-             <div class="activityprog"><progress class="prog border" id="" value="50" max="100"></progress></div>
-             </div>`;
-        }
-        for (let i = 1; i < goalsList.year.length; i++) {
-            console.log("runde" + 1);
-            showGoals += `<div class="flex activitiesTheme border">
-             <div><p>  ${goalsList.year[i].name}  </p></div>
-             <div class="activityprog"><progress class="prog border" id="" value="50" max="100"></progress></div>
-             </div>`;
-        }
-        for (let i = 1; i < goalsList.fiveYearPlan.length; i++) {
-            console.log("runde" + 1);
-            showGoals += `<div class="flex activitiesTheme border">
-            <div><p> ${goalsList.fiveYearPlan[i].name}  </p></div>
-            <div class="activityprog"><progress class="prog border" id="" value="50" max="100"></progress></div>
-            </div>`;
+    if (selectedGoal == 'weekly' || selectedGoal == 'all') {
+        for (let i = 0; i < goalsData.weekly.length; i++) {
+            loadedGoals += `<div>${goalsData.weekly[i].name}</div>`
         }
     }
-    document.getElementById("viewGoals").innerHTML = showGoals;
-    //return showGoals;
-}
+    if (selectedGoal == 'monthly' || selectedGoal == 'all') {
+        for (let i = 0; i < goalsData.monthly.length; i++) {
+            loadedGoals += `<div>${goalsData.monthly[i].name}</div>`
+        }
+    }
+    if (selectedGoal == 'year' || selectedGoal == 'all') {
+        for (let i = 0; i < goalsData.year.length; i++) {
+            loadedGoals += `<div>${goalsData.year[i].name}</div>`
+        }
+    }
+    if (selectedGoal == 'fiveYearPlan' || selectedGoal == 'all') {
+        for (let i = 0; i < goalsData.fiveYearPlan.length; i++) {
+            loadedGoals += `<div>${goalsData.fiveYearPlan[i].name}</div>`
+        }
+    }
 
-`<div class="flex activitiesTheme border">
-            <div><p> ${goalsList.fiveYearPlan[i].name}  </p></div>
-            <div class="activityprog"><progress class="prog border" id="" value="50" max="100"></progress></div>
-            </div>`;
-`<div class="flex activitiesTheme border">
-            <div><p>${myRepeatTasks[i]?.name}</p></div>
-            <div class="activityprog"><progress class="prog border" id="" value="50" max="100"></progress></div>
-            </div>`;
+    document.getElementById('viewGoals').innerHTML = loadedGoals;
+}
--- js/goalsTab/view.js
+++ js/goalsTab/view.js
@@ -1,65 +1,26 @@
-/*<option value='all'>Alle</option>
-            <option value='year'>Siste året</option>
-            <option value='halfYear'>Siste 6 mnd</option>
-            <option value='quarterYear'>Siste 3 mnd</option>
-            <option value='month'>Siste mnd</option> <!--${loadGoals(timeframe) || ''}-->*/
-
 function goalsView() {
     document.getElementById("dashcontent").innerHTML = /*HTML*/`
-      <div class="goalback styled-scrollbars">
-        <div class="goalwrapper ">
-
-            <div class="goalnavbar">
-                <button class="addbtnG" onclick="dashView()">Avslutt</button>
+        <div class="goalback styled-scrollbars">
+            <div class="goalwrapper ">
+                <div class="goalnavbar">
+                    <button class="addbtnG" onclick="dashView()">Avslutt</button>
                     <h1>Mine Mål</h1>
-                        <form id="goalForm" action="...">
-                            <label for="DDMgoals">Tidsrom:</label>
-                            <select name="mål" id="DDMgoals" onchange="selectTimeframe(this)">
-                            
-                            </select>
-                            <input type="submit" value="vis valgte"/>
-                        </form> <hr class="goalhr">
-</div>
-
-</div><div id="viewGoals" class="innergoalwrapper ">
-
-
-</div></div>
-
-       
-    `;
-    let timeKeys = model.interface.goals.timeframe;
-    let modelKeys = Object.keys(model.data.goalList);
-    console.log(modelKeys);
-    for (key in timeKeys) {
-        console.log(modelKeys[key - 1]);
-        console.log(timeKeys[key]);
-        if (timeKeys[key] == 'Alle') {
-            document.getElementById("DDMgoals").innerHTML += `
-    <option value="Alle">${timeKeys[key]}</option>`;
-        }
-        else {
-            document.getElementById("DDMgoals").innerHTML += `
-            <option value="${modelKeys[key - 1]}">${timeKeys[key]}</option>`;
-        }
-
-    }
-    const FormG = document.getElementById("goalForm");
-    FormG.addEventListener('submit', function (event) {
-        //forhindrer standard innsendelse av data fra form
-        event.preventDefault();
-
-        //bruk formData API til å fange data fra form
-        const FormGData = new FormData(this);
-
-        //konverter form data til et object
-        const FormGObject = Object.fromEntries(FormGData.entries());
-        console.log(FormGObject);
-
-
-        //nå kan det brukes i modellen ()?
-        processGoals(FormGObject, FormG);
-
-
-    })
+                    <form id="goalForm" action="...">
+                        <label for="DDMgoals">Tidsrom:</label>
+                        <select name="mål" id="DDMgoals" onchange="selectGoal(this)">
+                            <option value='all'>Alle mål</option>
+                            <option value='daily'>Dagens mål</option>
+                            <option value='weekly'>Ukens mål</option>
+                            <option value='monthly'>Måndtens mål</option>
+                            <option value='year'>Årets mål</option>
+                             <option value='fiveYearPlan'>Neste 5 års mål</option>
+                        </select>
+                    </form> <hr class="goalhr">
+                </div>
+            </div>
+        <div id="viewGoals" class="innergoalwrapper ">
+        </div>
+        </div>
+    `  
+    loadGoals();
 }
